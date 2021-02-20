e4b88eac5d76ff0fe5f56df9baf16c3784e06873
CowJaehoon <76734067+Jaehoon-So@users.noreply.github.com>
2021-02-21 03:11:39 +0900

    js

--- 18.함수와 일급객체/SymbolIterator.js
+++ 18.함수와 일급객체/SymbolIterator.js
@@ -1,6 +1,7 @@
 function multiply(x, y) {
     const iterator = arguments[Symbol.iterator]();
-    console.log(arguments.length);
+
+    console.log(arguments.length); // 3
     console.log(iterator.next());
     console.log(iterator.next());
     console.log(iterator.next());
@@ -9,3 +10,17 @@ function multiply(x, y) {
     return x * y;
 }
 multiply(1, 2, 3);
+// 가변인자 함수를 구현할대 arguments 객체가 유용하다.
+
+function sum(){
+    let res = 0;
+
+    for(let i = 0; i < arguments.length; i++){
+        res += arguments[i];
+    }
+    return res;
+}
+
+console.log(sum() );
+console.log(sum(2, 3) );
+console.log(sum(2, 3, 4) );
---
+++ 18.함수와 일급객체/length 프로퍼티.js
@@ -0,0 +1,9 @@
+function foo() {}
+console.log(foo.length);
+
+function bar(x) {}
+console.log(bar.length);
+
+function baz(x, z) {}
+console.log(baz .length)
+
